a42efc4e87f6eeb4888120a5e0279808bf9b008d
Petra Palovaara <petra.palovaara@outlook.com>
2024-06-23 19:09:59 +0300

    1.3 header ja content toimii

--- 1/kurssitiedot/src/App.jsx
+++ 1/kurssitiedot/src/App.jsx
@@ -1,37 +1,42 @@
-const Header = (header) => {
+const Header = ({ name }) => {
   return (
-      <h1>{header.name}</h1> 
+      <h1>{name}</h1> 
   )
 }
 
-const Content = (content) => {
+const Content = ({ course }) => {
   return (
-    <p>{content.name} {content.count}</p>
+    <p>{ course.name } { course.exercises }</p>
   )
 }
 
-const Total = (total) => {
+/* const Total = (total) => {
   return (
     <p>Number of exercises {total.count} </p>  
   )
-}
+} */
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <>
       <Header name={course} />
-      <Content name={part1} count={exercises1}/>
-      <Content name={part2} count={exercises2}/>
-      <Content name={part3} count={exercises3}/>
-      <Total count={exercises1+exercises2+exercises3}/>
+      <Content course={part1}/>
+      <Content course={part2}/>
+      <Content course={part3}/>
     </>
   )
 }
