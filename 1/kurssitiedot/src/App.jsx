b46ad8cf37e95024f999b097cded129aa8c144dd
Petra Palovaara <petra.palovaara@outlook.com>
2024-06-23 19:39:44 +0300

    1.4 content toimii

--- 1/kurssitiedot/src/App.jsx
+++ 1/kurssitiedot/src/App.jsx
@@ -4,43 +4,44 @@ const Header = ({ name }) => {
   )
 }
 
-const Content = ({ course }) => {
+const Content = ({ table }) => {
   return (
-    <p>{ course.name } { course.exercises }</p>
+    <>
+    { table.map(course => (
+      <p> {course.name} {course.exercises} </p>
+    ))}
+    </>
   )
 }
 
-const Total = (total) => {
+/* const Total = (total) => {
   return (
     <p>Number of exercises {total.count} </p>  
   )
-} 
+} */ 
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  console.log(part1.exercises + part2.exercises + part3.exercises)
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <>
       <Header name={course} />
-      <Content course={part1}/>
-      <Content course={part2}/>
-      <Content course={part3}/>
-      <Total count={part1.exercises + part2.exercises + part3.exercises} />
-    </>
+      <Content table={parts}/>
+   </>
   )
 }
 
