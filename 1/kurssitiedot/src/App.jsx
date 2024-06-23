ba194454ddca9edaf3042a01f3e1d3fb89642605
Petra Palovaara <petra.palovaara@outlook.com>
2024-06-23 19:53:22 +0300

    1.5 kaikki toimii

--- 1/kurssitiedot/src/App.jsx
+++ 1/kurssitiedot/src/App.jsx
@@ -14,39 +14,40 @@ const Content = ({ table }) => {
   )
 }
 
-const Total = ({ table }) => {
+ const Total = ({ table }) => {
   let sum = 0
   table.forEach(course => {
     sum = sum + course.exercises
-    console.log(sum)
   })
   return (
     <p>Number of exercises {sum}</p>  
   )
-}  
+}   
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
-      <Header name={course} />
-      <Content table={parts}/>
-      <Total table={parts} />
+      <Header name={course.name} />
+      <Content table={course.parts}/>
+      <Total table={course.parts} /> 
    </>
   )
 }
