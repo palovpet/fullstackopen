6b44bf561e152a2d3ba7cdf7559db6c97ce2c9ca
Petra Palovaara <petra.palovaara@outlook.com>
2024-07-22 19:53:23 +0300

    .

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,22 +1,44 @@
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
-    </div>
-  )
-}
+import { useState } from 'react'
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
-  const nimi = "Otso"
-  const ika = 0
+  const [counter, setCounter] = useState(0)
+
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
+
   return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name="Mikko" age={26+10}/>
-      <Hello name={nimi} age={ika}/>
-    </>
+    <div>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={setToZero} text="zero" />
+      <Button handleClick={decreaseByOne} text="minus" />
+    </div>
   )
-}
+} 
 
 export default App
