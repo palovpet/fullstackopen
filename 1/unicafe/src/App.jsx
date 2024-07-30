dc1ba5987839f51694167b9cd846f426a0ab7ac4
Petra Palovaara <petra.palovaara@outlook.com>
2024-07-30 19:10:20 +0300

    1.7 valmis

--- 1/unicafe/src/App.jsx
+++ 1/unicafe/src/App.jsx
@@ -5,10 +5,12 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [positives, setPositives] = useState(0)
 
   const handleGoodClick = () => {
     setAll(allClicks.concat(1))
     setGood(good + 1)
+    setPositives(positives + 1)
   }
 
   const handleNeutralClick = () => {
@@ -19,6 +21,25 @@ const App = () => {
   const handleBadClick = () => {
     setAll(allClicks.concat(-1))
     setBad(bad + 1)
+  }
+
+  const divide = function(a, b) {
+    if (b == 0){
+      return 0
+    }
+    return ((a / b)*100)
+  }
+
+  const average = function(t) {
+    if (t.length == 0){
+      return 0
+    }
+
+    let sum = 0
+    t.forEach(value => {
+      sum = sum + value
+    })
+    return sum/t.length
   }
 
   return (
@@ -33,10 +54,9 @@ const App = () => {
       <p> good {good} </p>
       <p> neutral {neutral} </p>
       <p> bad {bad}</p>
-      <p> all </p>
-      <p> average </p>
-      <p> positive </p>
-      <p>{allClicks.join('  ')}</p>
+      <p> all {allClicks.length}</p>
+      <p> average {average(allClicks)}</p>
+      <p> positive {divide(positives, allClicks.length)}%</p>
     </div>
   )
 }
