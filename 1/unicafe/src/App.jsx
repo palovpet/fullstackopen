143a2894aeb556943eff8cc601eb958e6c8c7e75
Petra Palovaara <petra.palovaara@outlook.com>
2024-07-30 18:31:05 +0300

    1.7 arvojen lisäys taulukkoon

--- 1/unicafe/src/App.jsx
+++ 1/unicafe/src/App.jsx
@@ -1,29 +1,42 @@
 import { useState } from 'react'
 
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleGoodClick = () => {
+    setAll(allClicks.concat(1))
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setAll(allClicks.concat(0))
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setAll(allClicks.concat(-1))
+    setBad(bad + 1)
+  }
 
   return (
     <div>
       <h3>give feedback</h3>
       <div>
-        <button onClick={() => setGood(good + 1)}>
-          good
-        </button>
-        <button onClick={() => setNeutral(neutral + 1)}>
-          neutral
-        </button>
-        <button onClick={() => setBad(good + 1)}>
-          bad
-        </button>
+        <button onClick={handleGoodClick}>good</button>
+        <button onClick={handleNeutralClick}>neutral</button>
+        <button onClick={handleBadClick}>bad</button>
       </div>
       <h3>statistics</h3>
       <p> good {good} </p>
       <p> neutral {neutral} </p>
       <p> bad {bad}</p>
+      <p> all </p>
+      <p> average </p>
+      <p> positive </p>
+      <p>{allClicks.join('  ')}</p>
     </div>
   )
 }
