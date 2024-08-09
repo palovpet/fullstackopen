0ba7057e90cb5ebcc63157b9b12144e9ca98286c
Petra Palovaara <petra.palovaara@outlook.com>
2024-08-09 18:22:59 +0300

    2.1 header toimii

--- 2/kurssitiedot/src/App.jsx
+++ 2/kurssitiedot/src/App.jsx
@@ -1,6 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
+/* const Total = ({ sum }) => <p>Number of exercises {sum}</p>
 
 const Part = ({ part }) => 
   <p>
@@ -18,32 +16,46 @@ const Content = ({ parts }) =>
     <Part
       part={parts[2]} 
     />      
-  </>
+  </> */
+
+const Course = ( {course} ) => {
+  const header = course.name
+
+  console.log(header)
+
+  return<h1>{header}</h1>
+
+}
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Course course={course} />
     </div>
   )
 }
+
 
 export default App
