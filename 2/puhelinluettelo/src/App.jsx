085b4890bd6d46d99113fdafe66b90b084126d5c
Petra Palovaara <petra.palovaara@outlook.com>
2024-08-18 12:01:50 +0300

    2.13 toimii

--- 2/puhelinluettelo/src/App.jsx
+++ 2/puhelinluettelo/src/App.jsx
@@ -1,5 +1,84 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/person'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('') 
+  const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
+  const getIndexorDefault = (list, value) => {
+    for (let i = 0; i < list.length; i++) {
+      if (list[i].name === value) {
+        return i 
+      }
+    }
+    return -1
+  }
+  
+  const addNameAndNumber = (event) => {
+    event.preventDefault()
+
+    if (getIndexorDefault(persons, newName) === -1) {
+    
+    const nameObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    personService
+      .create(nameObject)
+      .then(response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      })
+    
+    } else {
+      console.log('nimi löytyi jo')
+      window.alert(`${newName} löytyypi jo`)
+    } 
+  }
+
+  const handleChange = (event) => {
+    const {name, value} = event.target
+
+    if (name === "name") {
+      setNewName(value)
+    }
+    if (name === "number") {
+      setNewNumber(value)
+    }
+  }
+  
+  const filterNumbers = (event) => {
+    const { value } = event.target
+    setFilter(value)
+  }
+
+  const personsToShow = persons.filter(person =>
+    person.name.includes(filter)
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} filterNumbers={filterNumbers} />
+      <h3>add a new</h3>
+      <AddNew addNameAndNumber={addNameAndNumber} newName={newName} newNumber={newNumber} handleChange={handleChange} />
+      <h3>Numbers</h3>
+      <ShowPersons personsToShow={personsToShow}/>     
+    </div>
+  )
+}
 
 const Filter = ({ filter, filterNumbers}) => {
   return (
@@ -41,89 +120,6 @@ const ShowPersons = ({ personsToShow }) => {
         )}
       </div>
   )
-}
-
-const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-
-  const [newNumber, setNewNumber] = useState('') 
-
-  const [filter, setFilter] = useState('')
-
-  useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      })
-  }, [])
-
-  const getIndexorDefault = (list, value) => {
-    for (let i = 0; i < list.length; i++) {
-      if (list[i].name === value) {
-        return i 
-      }
-    }
-    return -1
-  }
-  
-  const addNameAndNumber = (event) => {
-    event.preventDefault()
-
-    if (getIndexorDefault(persons, newName) === -1) {
-    
-    const nameObject = {
-      name: newName,
-      number: newNumber
-    }
-    axios
-      .post('http://localhost:3001/persons', nameObject)
-      .then(response => {
-        setPersons(persons.concat(response.data))
-        setNewName('')
-        setNewNumber('')
-      })
-    setPersons(persons.concat(nameObject))
-    
-    } else {
-      console.log('nimi löytyi jo')
-      window.alert(`${newName} löytyypi jo`)
-    } 
-  }
-
-  const handleChange = (event) => {
-    const {name, value} = event.target
-
-    if (name === "name") {
-      setNewName(value)
-    }
-    if (name === "number") {
-      setNewNumber(value)
-    }
-  }
-  
-  const filterNumbers = (event) => {
-    const { value } = event.target
-    setFilter(value)
-  }
-
-  const personsToShow = persons.filter(person =>
-    person.name.includes(filter)
-  )
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter filter={filter} filterNumbers={filterNumbers} />
-      <h3>add a new</h3>
-      <AddNew addNameAndNumber={addNameAndNumber} newName={newName} newNumber={newNumber} handleChange={handleChange} />
-      <h3>Numbers</h3>
-      <ShowPersons personsToShow={personsToShow}/>     
-    </div>
-   
-  )
-
 }
 
 export default App
