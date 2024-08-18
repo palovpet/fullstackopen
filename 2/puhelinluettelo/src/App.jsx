2c67cea9786cb45adcf5a80248830eb0c0151eca
Petra Palovaara <petra.palovaara@outlook.com>
2024-08-18 12:45:26 +0300

    2.14 toimii

--- 2/puhelinluettelo/src/App.jsx
+++ 2/puhelinluettelo/src/App.jsx
@@ -68,6 +68,19 @@ const App = () => {
     person.name.includes(filter)
   )
 
+  const deletePerson = (person) => {
+    if (window.confirm(`Wish to delete ${person.name}?`)) {
+      personService
+        .deletePerson(person.id)
+        .then(() => {
+          setPersons(persons.filter(n => n.id !== person.id))
+        })
+        .catch(error => {
+          console.log('fail', error)
+        })    
+    }
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -75,14 +88,14 @@ const App = () => {
       <h3>add a new</h3>
       <AddNew addNameAndNumber={addNameAndNumber} newName={newName} newNumber={newNumber} handleChange={handleChange} />
       <h3>Numbers</h3>
-      <ShowPersons personsToShow={personsToShow}/>     
+      <ShowPersons personsToShow={personsToShow} deletePerson={deletePerson}/>     
     </div>
   )
 }
 
 const Filter = ({ filter, filterNumbers}) => {
   return (
-  <div> filtteröi 
+  <div> filter  
     <input 
       name="filter"
       value={filter}
@@ -112,11 +125,14 @@ const AddNew = ({ addNameAndNumber, newName, newNumber, handleChange }) => {
   )
 }
 
-const ShowPersons = ({ personsToShow }) => {
+const ShowPersons = ({ personsToShow, deletePerson }) => {
   return (
     <div>
         {personsToShow.map(person => 
-        <li key={person.name}>{person.name} {person.number}</li>
+        <li 
+          key={person.name}>{person.name} {person.number}
+          <button type="button" onClick={() => deletePerson(person)}>delete</button>
+          </li>
         )}
       </div>
   )
