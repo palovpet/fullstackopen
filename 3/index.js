782c3c9a3e827dd15290e5bed4c63fb03cb5f6b4
Petra Palovaara <petra.palovaara@outlook.com>
2024-11-26 18:18:37 +0200

    3.5 ja 3.6 toimii

--- 3/index.js
+++ 3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
       id: 1,
@@ -48,9 +50,42 @@ app.get('/info', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-  
+    persons = persons.filter(person => person.id !== id)  
     response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+
+    const notUnique = persons.some(person => person.content === body.content)
+    if (notUnique) {
+      return response.status(400).json({ 
+        error: 'name must be unique' 
+      })
+    }
+  
+    const person = {
+      id: generateId(),
+      content: body.content,
+      number: body.number, 
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(note)
   })
 
 const PORT = 3001
