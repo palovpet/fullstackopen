2b23bbb8c5cb31e73fcd7bba686b51eac7a2f2c9
Petra Palovaara <petra.palovaara@outlook.com>
2024-12-08 12:12:39 +0200

    3.10-3.11

--- 3_puhelinluettelo_fullstack/backend/index.js
+++ 3_puhelinluettelo_fullstack/backend/index.js
@@ -25,6 +25,8 @@ app.use(cors())
 
 app.use(express.json())
 
+app.use(express.static('dist'))
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World :)</h1>')
@@ -99,7 +101,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
   })
 
-  const PORT = process.env.PORT || 3002
+  const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
