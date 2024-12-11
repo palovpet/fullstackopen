1c0f50b61be7b00dcec5dab586cb19c077aa1628
Petra Palovaara <petra.palovaara@outlook.com>
2024-12-11 19:58:01 +0200

    4.2 toimii

--- 4/app.js
+++ 4/app.js
@@ -1,46 +1,30 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
-
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to ', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World <3</h1>')
   })
-
-// const blogSchema = mongoose.Schema({
-//   title: String,
-//   author: String,
-//   url: String,
-//   likes: Number
-// })
-
-// const Blog = mongoose.model('Blog', blogSchema)
-
-// const mongoUrl = process.env.MONGODB_URI
-// mongoose.connect(mongoUrl)
-
-// app.use(cors())
-// app.use(express.json())
-
-// app.get('/api/blogs', (request, response) => {
-//   Blog
-//     .find({})
-//     .then(blogs => {
-//       response.json(blogs)
-//     })
-// })
-
-// app.post('/api/blogs', (request, response) => {
-//   const blog = new Blog(request.body)
-
-//   blog
-//     .save()
-//     .then(result => {
-//       response.status(201).json(result)
-//     })
-// })
-
 
 module.exports = app
