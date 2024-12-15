cad6f9b8da4da31a531ee9894ec7ec3b8d82798d
Petra Palovaara <petra.palovaara@outlook.com>
2024-12-15 15:16:10 +0200

    4.8 toimii

---
+++ 4/tests/blog_api.test.js
@@ -0,0 +1,23 @@
+const { test, after } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are six blogs', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body.length, 6)
+})
+
+after(async () => {
+  await mongoose.connection.close()
+})
