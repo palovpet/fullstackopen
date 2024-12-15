817d97d3e6d84cf053a43799dd08ae31ec8483b9
Petra Palovaara <petra.palovaara@outlook.com>
2024-12-15 15:56:07 +0200

    4.9 toimii

--- 4/tests/blog_api.test.js
+++ 4/tests/blog_api.test.js
@@ -1,10 +1,48 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
 const Blog = require('../models/blog')
+
+const initialBlogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0
+  }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  let blogObject = new Blog(initialBlogs[0])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[1])
+  await blogObject.save()
+  blogObject = new Blog(initialBlogs[2])
+  await blogObject.save()
+
+})
 
 test('blogs are returned as json', async () => {
   await api
@@ -13,9 +51,16 @@ test('blogs are returned as json', async () => {
     .expect('Content-Type', /application\/json/)
 })
 
-test('there are six blogs', async () => {
+test('there are three blogs', async () => {
   const response = await api.get('/api/blogs')
-  assert.strictEqual(response.body.length, 6)
+  console.log('count ', response.body.length)
+  assert.strictEqual(response.body.length, initialBlogs.length)
+})
+
+test('id-field is named id', async () => {
+  const response = await api.get('/api/blogs')
+  assert.strictEqual(response.body[0]._id, undefined)
+  assert.ok(response.body[0].id)
 })
 
 after(async () => {
