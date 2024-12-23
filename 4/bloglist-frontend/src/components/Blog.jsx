887ffb1eed84be5e7ed608daa951e7ed1d6bced9
Petra Palovaara <petra.palovaara@outlook.com>
2024-12-23 10:36:11 +0200

    5.6, 5.7

--- 4/bloglist-frontend/src/components/Blog.jsx
+++ 4/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [showInfo, setShowInfo] = useState(false)
+  const hideWhenShowInfo = { display: showInfo ? 'none' : '' }
+  const showWhenShowInfo = { display: showInfo ? '' : 'none' }
+
+  const listStyle = {
+    paddingTop: 10,
+    paddingLeft: 3,
+    paddingBottom: 5,
+    border: 'solid',
+    borderColor: 'pink',
+    borderWidth: 3
+  }
+
+ return (
+  <div style={listStyle}>
+  <div style={hideWhenShowInfo}>
+   <p>{blog.title}</p>
+   <button id='view-button' onClick={ () => setShowInfo(true)}>view</button>
+  </div>
+  <div style={showWhenShowInfo}>
+    <p><b>title: </b>{blog.title}</p>
+    <p><b>author: </b> {blog.author}</p>
+    <p><b> likes: </b>{blog.likes}</p>
+    <p><b> url: </b>{blog.url}</p>
+    <button onClick={ () => setShowInfo(false)}>hide</button>
+  </div>
   </div>  
-)
+  )
+}
+  
 
 export default Blog
